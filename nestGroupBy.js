53da99e0840ee00d981b2a4b7fa15726fe821708
xiongliding <fairplay1986@gmail.com>
2020-07-15 09:38:03 +0800

    将 nestGroupBy 改成 commonjs 模式

--- nestGroupBy.js
+++ nestGroupBy.js
@@ -1,14 +1,14 @@
-const { map, groupBy } = require("rambda");
+const { map, groupBy } = require('rambda');
 
 // 多级分组
 const nestGroupBy = ([fn, ...fns], list) => {
-    if (!fn) {
-        return list;
-    }
-    const g = groupBy(fn, list);
-    return map(v => {
-        return nestGroupBy(fns, v);
-    }, g);
+  if (!fn) {
+    return list;
+  }
+  const g = groupBy(fn, list);
+  return map((v) => {
+    return nestGroupBy(fns, v);
+  }, g);
 };
 
-export default nestGroupBy;
+module.exports = nestGroupBy;
